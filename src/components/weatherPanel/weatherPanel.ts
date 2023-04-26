343402d139d91112994f209a2355b205e376dec4
igorlech <eaaigle@students.eaaa.dk>
2023-04-26 12:47:30 +0200

    input handler setup

--- src/components/weatherPanel/weatherPanel.ts
+++ src/components/weatherPanel/weatherPanel.ts
@@ -2,11 +2,21 @@ import './weatherPanel.scss';
 
 export class weatherPanel {
     constructor() {
-        this.fetchWeatherData();
+        const input = document.querySelector<HTMLInputElement>('.input');
+        const form = document.querySelector<HTMLFormElement>('.form');
+        const output = document.querySelector<HTMLDivElement>('.weather-output');
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            if (input?.value == '' || input?.value == null) return;
+
+            this.fetchWeatherData(input.value);
+            output.innerHTML = `<p>Weather for ${input.value}</p>`;
+        });
     }
 
-    fetchWeatherData() {
-        const city = 'London';
+    fetchWeatherData(city: string) {
         fetch(`https://goweather.herokuapp.com/weather/${city}`)
             .then((response) => response.json())
             .then((data) => console.log(data));
