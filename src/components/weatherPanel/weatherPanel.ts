9aa4c117d934a8932c4885399b4960dffb1820dd
igorlech <eaaigle@students.eaaa.dk>
2023-04-26 12:24:02 +0200

    test api fetch

--- src/components/weatherPanel/weatherPanel.ts
+++ src/components/weatherPanel/weatherPanel.ts
@@ -1,5 +1,14 @@
 import './weatherPanel.scss';
 
 export class weatherPanel {
+    constructor() {
+        this.fetchWeatherData();
+    }
 
+    fetchWeatherData() {
+        const city = 'London';
+        fetch(`https://goweather.herokuapp.com/weather/${city}`)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
 }
