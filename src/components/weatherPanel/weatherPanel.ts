34f53163d643b45493b7dd7042c23bfd8a659217
igorlech <eaaigle@students.eaaa.dk>
2023-04-28 21:55:58 +0200

    feat: initial logic for rendering the data

--- src/components/weatherPanel/weatherPanel.ts
+++ src/components/weatherPanel/weatherPanel.ts
@@ -1,10 +1,14 @@
 import './weatherPanel.scss';
 
+interface MyData {
+    temperature: string;
+    wind: string;
+    description: string;
+}
 export class weatherPanel {
     constructor() {
         const input = document.querySelector<HTMLInputElement>('.input');
         const form = document.querySelector<HTMLFormElement>('.form');
-        const output = document.querySelector<HTMLDivElement>('.weather-output');
 
         form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -12,13 +16,34 @@ export class weatherPanel {
             if (input?.value == '' || input?.value == null) return;
 
             this.fetchWeatherData(input.value);
-            output.innerHTML = `<p>Weather for ${input.value}</p>`;
         });
     }
 
+    // TODO: Make fetch async
+    // TODO: Check for better API, current is missing data for some cities
+    // in case of error, display error message
     fetchWeatherData(city: string) {
         fetch(`https://goweather.herokuapp.com/weather/${city}`)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.renderWeatherData(data as MyData));
+    }
+
+    renderWeatherData(data: MyData) {
+        const output = document.querySelector<HTMLDivElement>('.weather-output');
+        if (data.temperature != null) {
+            output.innerHTML = '';
+            output.innerHTML += `
+            <div class="weather-data">
+                <p>Temperature: ${data.temperature}</p>
+                <p>Wind: ${data.wind}</p>
+                <p>Description: ${data.description}</p>
+            </div>`;
+        } else {
+            output.innerHTML = '';
+            output.innerHTML += `
+            <div class="weather-data">
+                <p>Sorry, your city is currently not on our list</p>
+            </div>`;
+        }
     }
 }
