02a684d99cd6aa280dd96e578bd6117931046ed2
igorlech <eaaigle@students.eaaa.dk>
2023-05-07 12:47:44 +0200

    cleanup and initial output styling; fetch as async

--- src/components/weatherPanel/weatherPanel.ts
+++ src/components/weatherPanel/weatherPanel.ts
@@ -15,39 +15,32 @@ export class weatherPanel {
         });
     }
 
-    // TODO: Make fetch async
-    // TODO: Check if the new API is working
-
-    fetchWeatherData(city: string, key: string) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
+    async fetchWeatherData(city: string, key: string) {
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
             .then((response) => response.json())
             .then((data) => this.renderWeatherData(data as WeatherData));
     }
 
     renderWeatherData(data: WeatherData) {
         const output = document.querySelector<HTMLDivElement>('.weather-output');
+        output.style.display = 'block';
 
         output.innerHTML = '';
 
         output.innerHTML += `
         <div class="weather-data">
-            <p>Temperature: ${data.main.temp}</p>
-            <p>Wind: ${data.wind.speed}</p>
-            <p>Humidity: ${data.main.humidity}</p>
-            <p>Pressure: ${data.main.pressure}</p>
-            <p>Clouds: ${data.clouds.all}</p>
-            <p>Country: ${data.sys.country}</p>
-            <p>Sunrise: ${data.sys.sunrise}</p>
-            <p>Sunset: ${data.sys.sunset}</p>
-            <p>Visibility: ${data.visibility}</p>
             <p>City: ${data.name}</p>
+            <p>Country: ${data.sys.country}</p>
+            <p>Main: ${data.weather[0].main} - ${data.weather[0].description}</p>
+            <p>Temperature: ${Math.round(data.main.temp)} °C</p>
+            <p>Wind: ${data.wind.speed} m/s</p>
+            <p>Humidity: ${data.main.humidity}</p>
+            <p>Pressure: ${data.main.pressure} hPa</p>
+            <p>Clouds: ${data.clouds.all}%</p>
+            <p>Visibility: ${data.visibility}</p>
 
-            <div class="weather-dat-main">
-                <p>Weather:</p>
-                <p>Id: ${data.weather[0].id}</p>
-                <p>Main: ${data.weather[0].main}</p>
-                <p>Description: ${data.weather[0].description}</p>
-                <p>Icon: ${data.weather[0].icon}</p>
+            <div class="weather-data-icon">
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather icon">
             </div>
         </div>`;
     }
